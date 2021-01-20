ebcea16f56a9f99aca3581909cd36ac9437dfdf8
Mihail83 <Sklif83@tut.by>
2021-01-20 17:20:27 +0300

    SomeErrorInExeptionAndCSS

--- HWThreeLayerShop.DAL/Repositories/ShopContextRepository.cs
+++ HWThreeLayerShop.DAL/Repositories/ShopContextRepository.cs
@@ -21,9 +21,9 @@ namespace HWThreeLayerShop.DAL.Repositories
 
         public List<CatalogDTO> GetAllCatalogDTOs()
         {
-            var catalogs = _context.Catalogs;
+            var catalogs = _context.Catalogs.ToList();
             var catalogsList = new List<CatalogDTO>();
-            if (catalogs != null)
+            if (catalogs != null && catalogs.Count()!=0)
             {
                 catalogsList.AddRange(catalogs.Select(catalog => new CatalogDTO { Id = catalog.Id, Name = catalog.Name }));
             }
@@ -37,10 +37,17 @@ namespace HWThreeLayerShop.DAL.Repositories
         public List<GoodDTO> GetGoodDTOsByCataligId(int id)
         {
             List<GoodDTO> listGoodDTO = new List<GoodDTO>();
-            var listGoodsByCatalogId = _context.Goods.Include("Catalogs").Where(p => p.Catalogs.Any(p=>p.Id==id));
+            var listGoodsByCatalogId = _context.Goods.Include("Catalogs").Where(p => p.Catalogs.Any(p=>p.Id==id)).ToList();
 
-            if (listGoodsByCatalogId == null) throw new NotFoundExeption("Товары отсутствуют");                            
-            listGoodDTO = listGoodsByCatalogId.Select(good => new GoodDTO(good.Id, good.Name, good.Price, good.Catalogs.Select(cat => cat.Id))).ToList<GoodDTO>();            
+            if (listGoodsByCatalogId != null && listGoodsByCatalogId.Count()!=0)
+            {
+                listGoodDTO = listGoodsByCatalogId.Select(good => new GoodDTO(good.Id, good.Name, good.Price, good.Catalogs.Select(cat => cat.Id))).ToList<GoodDTO>();
+            }
+            else
+            {
+                throw new NotFoundExeption("Товары отсутствуют");
+            }
+
             return listGoodDTO;
         }
     }
